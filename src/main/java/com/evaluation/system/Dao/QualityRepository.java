74158858d7fe6fc1d8f0d55d67419e3a0648d761
sky-cloud-debug <1115178050@qq.com>
2020-01-18 20:08:26 +0800

    2020.1.18 宋

--- src/main/java/com/evaluation/system/Dao/QualityRepository.java
+++ src/main/java/com/evaluation/system/Dao/QualityRepository.java
@@ -4,5 +4,8 @@ import com.evaluation.system.domain.quality;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QualityRepository extends JpaRepository<quality,Integer> {
+    public quality findBywisdom(double asd);
+
+    public quality findByClassMajor(String major);
 
 }
