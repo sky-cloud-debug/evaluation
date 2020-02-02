5df33bbc0a2183ad93a5d304cb8ed00841cecc2b
sky-cloud-debug <1115178050@qq.com>
2020-02-02 10:13:09 +0800

    2020.2.2 宋

--- src/main/java/com/evaluation/system/Conterllor/MainConterllor.java
+++ src/main/java/com/evaluation/system/Conterllor/MainConterllor.java
@@ -1,6 +1,8 @@
 package com.evaluation.system.Conterllor;
 
+import com.evaluation.system.Service.fileService;
 import com.evaluation.system.Service.loginService;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+import java.util.List;
 
 @Controller
 @RequestMapping("/main")
@@ -17,6 +24,9 @@ public class MainConterllor {
 
     @Autowired
     loginService loginService;
+
+    @Autowired
+    fileService fileService;
 
     @GetMapping("index")
     public String login(){return "login/Login";}
@@ -40,6 +50,30 @@ public class MainConterllor {
             return "login/Login";
         }
         return "index";
+    }
+
+    @PostMapping("/upfile")
+    public String upfile(Model model,HttpServletRequest request) throws Exception{
+        //每次上传某个excel前先将其在数据库中的对应的数据表的数据清空，此处没写，需要根据具体需要来写
+
+        MultipartHttpServletRequest multipartHttpServletRequest=(MultipartHttpServletRequest) request;
+
+
+        MultipartFile file= multipartHttpServletRequest.getFile("filename");
+        String filename=file.getOriginalFilename();
+        String type=filename.substring(filename.indexOf(".")+1);
+        if(!type.equals("xls")||type.equals("xlsx")){
+            model.addAttribute("msg","文件类型只能是xls或者xlsx格式");
+            return "";
+        }
+        if(file.isEmpty()){
+            model.addAttribute("msg","文件不能为空");
+            return "";
+        }
+        InputStream inputStream=file.getInputStream();
+        List<List<Object>> list=fileService.getExcel(inputStream,file.getOriginalFilename());
+
+        return "";
     }
 
 }
