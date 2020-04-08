7cfab12e1c1bad7f5516546ad5a87923278e67c3
jyx1244204317 <1244204317@qq.com>
2020-04-09 01:25:56 +0800

    2020-4-9 江 管理员审核加分项js部分完成

--- src/main/java/com/evaluation/system/Dao/AwardTempRepository.java
+++ src/main/java/com/evaluation/system/Dao/AwardTempRepository.java
@@ -7,10 +7,12 @@ import java.util.ArrayList;
 
 public interface AwardTempRepository extends JpaRepository<AwardTemp, String> {
 
-    public ArrayList<AwardTemp> findByClassMajorAndJudge(String classMajor,String judge);
+    public ArrayList<AwardTemp> findByClassMajorAndJudge(String classMajor, String judge);
 
     public ArrayList<AwardTemp> findByJudge(String judge);
 
     public ArrayList<AwardTemp> findByNumber(String number);
+
+    public AwardTemp findByNameAndAwardName(String name, String awardName);
 
 }
